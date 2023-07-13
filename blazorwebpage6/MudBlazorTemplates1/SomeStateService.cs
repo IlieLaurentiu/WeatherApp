21a4f684b495116df1be8eef0ad94887dc841fb2
Laurentiu Ilie <ilielaurentiu8@yahoo.com>
2023-07-13 05:18:40 +0300

    Added Webpage

---
+++ blazorwebpage6/MudBlazorTemplates1/SomeStateService.cs
@@ -0,0 +1,24 @@
+﻿namespace MudBlazorTemplates1
+{
+    public class SomeStateService
+    {
+        public bool isFahrenheit { get; set; }
+        public event Func<object, Task> RefreshView;
+
+        public async Task ToggleFahrenheit()
+        {
+            isFahrenheit = !isFahrenheit;
+            Console.WriteLine($"Some State is now {isFahrenheit}");
+            await TriggerRefreshView();
+        }
+        public Task TriggerRefreshView() => TriggerEventAsync(RefreshView);
+
+        private async Task TriggerEventAsync(Func<object, Task> eventHandler)
+        {
+            if (eventHandler != null)
+            {
+                await eventHandler.Invoke(this);
+            }
+        }
+    }
+}
